2858f3767b03eef0f8c8e2ee2fbbd1ce161458b1
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-04-27 18:04:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
@@ -26,27 +27,6 @@ export default defineConfig({
   build: {
     minify: 'esbuild',
     sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom', 'redux', 'react-redux'],
-          codemirror: [
-            '@uiw/react-codemirror',
-            '@codemirror/lang-javascript',
-            '@codemirror/lang-python',
-            '@codemirror/lang-css',
-            '@codemirror/lang-html',
-            '@codemirror/lang-markdown',
-            '@codemirror/commands',
-            '@codemirror/view',
-            '@codemirror/autocomplete',
-            '@codemirror/theme-one-dark',
-            '@uiw/codemirror-theme-dracula',
-          ],
-          animations: ['framer-motion'],
-        },
-      },
-    },
     outDir: 'dist',
     assetsDir: 'assets',
     commonjsOptions: {
